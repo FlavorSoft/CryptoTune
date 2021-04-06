4e1255922dc45a42cd3da5039efc2b8b75c72c4b
FlavorSoft <dodo87@freenet.de>
2021-04-06 18:51:38 +0200

    changed start.bat to better fit a wider range of GPUs

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 REM parameterlist mode, devIds, fanSpeeds, steps, nbrOfShares, nbrOfDatapoints, marginInMH, coreUCs, memOCs, powerLimits
 pip install -r requirements.txt
-python run.py --mode 1 --devices 0 --fans 71 --steps 5 --shares 3 --datapoints 20 --offset 0.3 --coreUC -220 --memOC 1100 --powerLimit 250
+python run.py --mode 1 --fans 71 --steps 5 --shares 3 --datapoints 20 --offset 0.3 --coreUC -50 --memOC 300
