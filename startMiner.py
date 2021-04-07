307cdf1046f1aeb43f1f40e1b3111b9a1e723d38
FlavorSoft <dodo87@freenet.de>
2021-04-07 13:46:05 +0200

    change fan and OC to GPU for unix

--- startMiner.py
+++ startMiner.py
@@ -77,7 +77,8 @@ class StartMiner:
             cmd = "powershell"
             self.proc = subprocess.Popen([cmd, self.exePath, parameters], creationflags = subprocess.CREATE_NEW_CONSOLE)
         else:
-            command = "sudo " + self.exePath + " " + parameters
+            subprocess.call(["chmod", "a+x", self.exePath])
+            command = "self.exePath" + " " + parameters
             self.log.Debug("Command: %s" % command)
             self.proc = subprocess.Popen(command.split(" "), shell=True)
         
