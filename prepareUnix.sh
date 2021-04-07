5fa7de4fcdf50d57f4abdab45cbebb262907d0c2
FlavorSoft <dodo87@freenet.de>
2021-04-07 13:39:35 +0200

    change fan and OC to GPU for unix

--- prepareUnix.sh
+++ prepareUnix.sh
@@ -3,4 +3,4 @@ sudo nvidia-xconfig --cool-bits=31 --allow-empty-initial-configuration
 sudo xinit &
 export DISPLAY=:0.0
 sudo nvidia-smi -pm 1
-sudo nvidia-settings -c :0 &
+echo -ne '\n' | sudo nvidia-settings -c :0 &
