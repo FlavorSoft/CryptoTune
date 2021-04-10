f53c3baacad4311e77b4f46590079c49da2f005d
FlavorSoft <dodo87@freenet.de>
2021-04-10 17:57:46 +0200

    fixed start parameters

--- optimize-profitability.bat
+++ optimize-profitability.bat
@@ -1,3 +1,3 @@
 pushd %~dp0
 pip install -r requirements.txt
-python run.py --mode 2 --loadPreset --dollarPerMHash 0.0966 --powercost 0.36
+python run.py --mode 2 --loadPreset --dollarPerMHash 0.0966 --powerCost 0.36
