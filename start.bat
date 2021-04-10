8f3a737a65dc3b56c645fbfbead399befebd7227
FlavorSoft <dodo87@freenet.de>
2021-04-10 17:56:43 +0200

    added switch to current directory into batch files

--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
+pushd %~dp0
 pip install -r requirements.txt
 python run.py --mode 2 --fans 71 --steps 5 --shares 3 --datapoints 20 --offset 0.3 --coreUC -50 --memOC 200 --powerLimit 240 --powerCost 0.36 --dollarPerMHash 0.096 --loadPreset
