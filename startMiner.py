4da0ef3e250774f28a5514e8a1ceb65d09f1b069
FlavorSoft <dodo87@freenet.de>
2021-04-15 17:41:20 +0200

    fixed parameter for unix

--- startMiner.py
+++ startMiner.py
@@ -60,7 +60,7 @@ class StartMiner:
         #print (configPath)
         parameters = self.config["parameters"].replace("#core#", str(coreUCs)).replace("#mem#", str(memOCs)).replace("#fan#", str(self.fans)).replace("#dev#", str(self.devIds)).replace("#worker#", self.workerName).replace("#port#", str(self.config["api"]["port"])).replace("#host#", self.config["api"]["host"]).replace("#ALGO#", algo)
         if not self.isWindows:
-            parameters = self.config["parametersUnix"].replace("#core#", str(coreUCs)).replace("#mem#", str(memOCs)).replace("#fan#", str(self.fans)).replace("#dev#", str(self.devIds)).replace("#worker#", self.workerName)
+            parameters = self.config["parametersUnix"].replace("#core#", str(coreUCs)).replace("#mem#", str(memOCs)).replace("#fan#", str(self.fans)).replace("#dev#", str(self.devIds)).replace("#worker#", self.workerName).replace("#ALGO#", algo)
         
         self.StartProcess(parameters)
         self.log.Debug(parameters)
