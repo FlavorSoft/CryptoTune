2a37ab231abcfe620e0d7d7220eb22822ad37ae6
FlavorSoft <dodo87@freenet.de>
2021-04-15 17:56:51 +0200

    fixed parameter for unix

--- prepareUnix.sh
+++ prepareUnix.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=28 -o nvidia-xorg.conf
+nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=28 -o nvidia-xorg.conf --enable-all-gpus
 sudo tmux new -d 'sudo X -config nvidia-xorg.conf :1'
 export DISPLAY=:1
-nvidia-settings -a [gpu:0]/GPUFanControlState=1
